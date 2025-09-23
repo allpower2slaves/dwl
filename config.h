dc755c7cae750726fffefb303500720ed59f0b71
allpower2slaves <allpower2slaves@users.noreply.github.com>
2025-09-23 14:55:48 +0300

    changed mouse bindings -- super+mwheel will kill the client

--- config.h
+++ config.h
@@ -247,6 +247,6 @@ static const Key keys[] = {
 
 static const Button buttons[] = {
 	{ MODKEY, BTN_LEFT,   moveresize,     {.ui = CurMove} },
-	{ MODKEY, BTN_MIDDLE, togglefloating, {0} },
+	{ MODKEY, BTN_MIDDLE, killclient, {0} },
 	{ MODKEY, BTN_RIGHT,  moveresize,     {.ui = CurResize} },
 };
